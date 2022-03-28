63beac4bb9e3746c97e0712904031ca2b94c0b1c
Emmanuel Ugochukwu <favour879@gmail.com>
2022-03-28 03:25:11 +0100

    Changed things

--- automate_download.py
+++ automate_download.py
@@ -1,5 +1,11 @@
+
+## Load Libraries
 import pandas as pd
 from dateutil.parser import parse
+import gdown
+import re
+
+## Custom Functions
 
 def is_date(string, fuzzy=False):
     """
@@ -13,17 +19,6 @@ def is_date(string, fuzzy=False):
 
     except ValueError:
         return False
-# # downloading files from google drive
-# import gdown
-# url = 'https://drive.google.com/drive/folders/1Ik_DZsRcyPwa7hD-LTqwkJbUUsO_U_mM?usp=sharing'
-# output = 'WhatsApp Chat with Young Data Professionals.txt'
-# gdown.download(url, output, quiet=False)
-
-# downloading files in folders from google drive
-import gdown
-url = 'https://drive.google.com/drive/folders/15BGSF7Xvv4nbd9rVI9ooJSToaAWLQAx5?usp=sharing'
-files = gdown.download_folder(url)
-
 
 def convert_whatsapp_messages_to_list(f):
     """
@@ -42,113 +37,162 @@ def convert_whatsapp_messages_to_list(f):
     return lines
 
 
-# Open the file and then call the function to convet the messages to list
-for file in files:
-    f = open(file, encoding = "utf8")
-    messages = convert_whatsapp_messages_to_list(f)
+def convert_chat_list_to_df(chat_list):
+    
+    # change the format to dataframe
+    messages_df = pd.DataFrame(chat_list,columns=['date','message'])
 
-# change the format to dataframe
-messages_df = pd.DataFrame(messages,columns=['date','message'])
+    # Extraction of the DataFrame's Time and Message 
+    time_msg = messages_df["message"].str.split("-", n = 1, expand = True)
+    messages_df["time"] = time_msg[0]
+    messages_df["message"] = time_msg[1]
 
-# Extraction of the DataFrame's Time and Message 
-time_msg = messages_df["message"].str.split("-", n = 1, expand = True)
-messages_df["time"] = time_msg[0]
-messages_df["message"] = time_msg[1]
+    #Extraction of the User Who Posted the Message posted and The Actual Message
+    user_msg = messages_df["message"].str.split(":", n = 1, expand = True)
+    messages_df["author"] = user_msg[0]
+    messages_df["message"] = user_msg[1]
 
-#Extraction of the User Who Posted the Message posted and The Actual Message
-user_msg = messages_df["message"].str.split(":", n = 1, expand = True)
-messages_df["author"] = user_msg[0]
-messages_df["message"] = user_msg[1]
+    messages_df['id'] = range(1, 1+len(messages_df))
 
-messages_df['id'] = range(1, 1+len(messages_df))
+    # Create a column for timestamp
+    messages_df['datetime'] = pd.to_datetime(messages_df.date + messages_df.time, errors='coerce', dayfirst=True)
+    messages_df = messages_df[["id","datetime","author","message"]]
 
-# Create a column for timestamp
-messages_df["timestamp"] = (messages_df["date"] +' ' + messages_df["time"])
-messages_df = messages_df[["id","date","time","timestamp","author","message"]]
+    return messages_df
 
-import re
+def filter_data(df, last_record):
 
-def extract_url(message):
+    new_records = df[df.datetime > last_record[0]]
+
+    latest_date = max(new_records.datetime)
+            
+    with open('data/latest_date.txt', 'w') as f:
+        f.write(str(latest_date))
+
+    print("New Date saved successfully")
+
+    return new_records
+
+
+def extract_url(df):
+
     """
     Regex to Extract URLs from Messages (Stackoverflow Help :smiley:)
     """
     url = r"(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)"
-    urls_extract = messages_df.message.str.extractall(url)
-    url_id = []
+    urls_extract = df.message.str.extractall(url)
 
-    for i in range(0,len(urls_extract)):
-    
-        add_list = urls_extract.index[[i]][0][0] + 1
-    
-        url_id.append(add_list)
-    urls_extract['id'] = url_id
+    if len(urls_extract) == 0:
+        print("No link in this batch")
 
-    urls_extract.head()
-    return urls_extract
+    else:
 
-links = extract_url(messages_df["message"])
+        url_id = []
 
-# merging the daframe containinng the imessages and those containing links
-data = pd.merge(messages_df, links, on = "id", how = "inner")
+        for i in range(0,len(urls_extract)):
+        
+            add_list = urls_extract.index[[i]][0][0] + 1
 
-data.columns = ['id', 'date', 'time', 'timestamp','author', 'message', 'url']
-data.to_csv("data/results.csv")
+            url_id.append(add_list)
 
-# search for words and then put those links inside a folder
+        urls_extract['id'] = url_id
+
+        links_tbl = pd.merge(df, urls_extract, on = "id", how = "inner")
+
+        links_tbl.columns = ['id', 'datetime', 'author', 'message', 'url']
+
+    return links_tbl
+
 def search(regex: str, df, case=False):
-    """Search all the text columns of `df`, return rows with any matches."""
-    textlikes = df.select_dtypes(include=[object, "string"])
-    return df[
-        textlikes.apply(
-            lambda column: column.str.contains(regex, regex=True, case=case, na=False)
-        ).any(axis=1)
-    ]
-# save the message and link to a folder
-data_analytics = search("data analytics", data)
-data_analytics = data_analytics[['url', 'timestamp','message']]
-data_analytics = data_analytics.reset_index()
-data_analytics.drop(columns = "index", inplace = True)
-data_analytics.head()
-data_analytics.to_csv("data/Data analytics.csv", index=False)
+                """Search all the text columns of `df`, return rows with any matches."""
+                textlikes = df.select_dtypes(include=[object, "string"])
+                return df[
+                    textlikes.apply(
+                        lambda column: column.str.contains(regex, regex=True, case=case, na=False)
+                    ).any(axis=1)
+                ]
 
-data_engineering = search("data engineering", data)
-data_engineering = data_engineering[['url','timestamp', 'message']]
-data_engineering = data_engineering.reset_index()
-data_engineering.drop(columns = "index", inplace = True)
-data_engineering.to_csv("data/Data engineering.csv")
+def save_types(df, key_word, type):
+    filtered_data = search(key_word, df).drop_duplicates()
 
-machine_learning = search("machine learning", data)
-machine_learning = machine_learning[['url','timestamp','message']]
-machine_learning = machine_learning.reset_index()
-machine_learning.drop(columns = "index", inplace = True)
-machine_learning.to_csv("data/Machine learning.csv")
+    if(len(filtered_data) == 0):
+        print(f"No links for {key_word} ")
 
-finance = search("onyeka", data)
-finance = finance[['url', 'timestamp', 'message']]
-finance = finance.reset_index()
-finance.drop(columns = "index", inplace = True)
-finance.to_csv("data/Finance.csv")
+    else:
+        filtered_data = filtered_data[['datetime','url','message']]
+        filtered_data = filtered_data.reset_index()
+        filtered_data.drop(columns = "index", inplace = True)
 
-data_analysis = search("data analysis", data)
-data_analysis = data_analysis[['url', 'timestamp', 'message']]
-data_analysis = data_analysis.reset_index()
-data_analysis.drop(columns = "index", inplace = True)
-data_analysis.to_csv("data/Data analysis.csv")
+        start = str(min(filtered_data.datetime)).split(" ")[0]
+                    
+        end = str(max(filtered_data.datetime)).split(" ")[0]
 
-power_bi = search("powerbi", data)
-power_bi = power_bi[['url','timestamp', 'message']]
-power_bi = power_bi.reset_index()
-power_bi.drop(columns = "index", inplace = True)
-power_bi.to_csv("data/PowerBi.csv")
+        filtered_data.to_csv(f"data/Extracted Links/{type}/{start}to{end}.csv", index=False)
 
-MS_excel = search("excel", data)
-MS_excel = MS_excel[['url','timestamp','message']]
-MS_excel = MS_excel.reset_index()
-MS_excel.drop(columns = "index", inplace = True)
-MS_excel.to_csv("data/MS excel.csv")
+        print(f"File saved to data/Extracted Links/{type}/{start}to{end}.csv")
 
-SQL_file = search("sql", data)
-SQL_file = SQL_file[['url','timestamp', 'message']]
-SQL_file = SQL_file.reset_index()
-SQL_file.drop(columns = "index", inplace = True)
-SQL_file.to_csv("data/SQL file.csv")
+def export_link(df):
+    
+    start = str(min(df.datetime)).split(" ")[0]
+                
+    end = str(max(df.datetime)).split(" ")[0]
+    
+    df.to_csv(f"data/Extracted Links/Main/{start}to{end}.csv", index=False)
+
+    print(f"Saved sucessfully to data/Extracted Links/Main/{start}to{end}.csv")
+
+## 
+
+## Load Whatsapp chat saved in google drive
+try:
+    url = 'https://drive.google.com/drive/folders/1YRKcXfLnFtQBTq68O4QiaUwtN7dz-Y8p?usp=sharing'
+    
+    files = gdown.download_folder(url)
+
+except:
+    print("Error in loading data")
+
+else:
+
+    try: 
+        for file in files:
+            f = open(file, encoding = "utf8")
+            messages = convert_whatsapp_messages_to_list(f)
+
+        last_date = open("data/latest_date.txt", encoding = "utf8")
+        last_record = last_date.readlines()
+
+    except:
+        print("Error in reading records")
+    else:
+        print("Reading whatsapp data and last date")
+        
+        message_df = convert_chat_list_to_df(messages)
+
+        if max(message_df.datetime) <= pd.to_datetime(last_record[0], yearfirst=True):
+
+            print("Records are upto date")
+
+        else:
+
+            filtered_df = filter_data(message_df, last_record)
+
+            links = extract_url(filtered_df)
+
+            export_link(links)
+
+            groups = ["data analytics", "data engineering", "machine learning",
+                        "onyeka", "data analysis", "powerbi|power bi",
+                        "excel", "sql"]
+
+            save_to = ["data analytics", "data engineering", "machine learning",
+                        "finance", "data analysis", "powerbi",
+                        "ms excel", "sql"]
+
+            if (len(groups) == len(save_to)):
+
+                for i in range(0,len(groups)):
+                    save_types(links, groups[i], save_to[i])
+            else:
+                print("groups is not equal to save_to")
+
